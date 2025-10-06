3c988ac0e635354f6b19908ebc05264ba75f71a9
fengwencheng66 <116816727+fengwencheng66@users.noreply.github.com>
2025-10-06 14:42:01 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 import recommonmark
 
@@ -28,8 +28,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-# html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
+ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 from recommonmark.parser import CommonMarkParser
